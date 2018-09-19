b2d71351e07ddc3ced22e8113a6493cd1b1eda00
Denys Pasishnyi <dpcat237@gmail.com>
2018-09-19 15:58:37 +0300

    Add proverb

---
+++ proverb/proverb.go
@@ -0,0 +1,27 @@
+package proverb
+
+import "fmt"
+
+const (
+	fraseTemplate = "For want of a %s the %s was lost."
+	endTemplate   = "And all for the want of a %s."
+)
+
+// Proverb generates relevant proverbs
+func Proverb(rhyme []string) []string {
+	var generated []string
+	total := len(rhyme)
+	if total < 1 {
+		return generated
+	}
+
+	for i := 0; i < total; i++ {
+		if i < (total - 1) {
+			generated = append(generated, fmt.Sprintf(fraseTemplate, string(rhyme[i]), string(rhyme[i+1])))
+			continue
+		}
+		generated = append(generated, fmt.Sprintf(endTemplate, rhyme[0]))
+	}
+
+	return generated
+}
