1bbc1d92dbaedf4c3982ae415e6cf1c636e30781
Denys Pasishnyi <dpcat237@gmail.com>
2018-10-28 16:37:05 +0200

    Add pangram

---
+++ pangram/pangram.go
@@ -0,0 +1,32 @@
+package pangram
+
+import "strings"
+
+func IsPangram(str string) bool {
+	if len(str) < 24 {
+		return false
+	}
+
+	pgm := make(map[int]bool)
+	for _, r := range strings.ToLower(str) {
+		n := int(rune(r))
+		if isValid(n) {
+			pgm[n] = true
+		}
+	}
+
+	return hasAll(pgm)
+}
+
+func isValid(n int) bool {
+	return n >= 97 && n <= 122
+}
+
+func hasAll(pgm map[int]bool) bool {
+	for i := 97; i <= 122; i++ {
+		if !pgm[i] {
+			return false
+		}
+	}
+	return true
+}
