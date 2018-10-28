01f26095b4b7e6c5b4ebecb7fde08164b3a43cd0
Denys Pasishnyi <dpcat237@gmail.com>
2018-10-28 18:07:14 +0200

    Add anagram

---
+++ anagram/anagram.go
@@ -0,0 +1,45 @@
+package anagram
+
+import (
+	"sort"
+	"strings"
+)
+
+func Detect(subject string, candidates []string) []string {
+	var result []string
+	for _, candidate := range candidates {
+		lSub := strings.ToLower(subject)
+		lCan := strings.ToLower(candidate)
+		if lSub == lCan {
+			continue
+		}
+		if len(lSub) != len(lCan) {
+			continue
+		}
+		if equalInts(normalize(lSub), normalize(lCan)) {
+			result = append(result, candidate)
+		}
+	}
+	return result
+}
+
+func normalize(subject string) []int {
+	var nums []int
+	for _, r := range subject {
+		nums = append(nums, int(r))
+	}
+	sort.Ints(nums)
+	return nums
+}
+
+func equalInts(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
