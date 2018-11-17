3b96678d16516157680cd1a5fa0260c7a1dba0cf
Denys Pasishnyi <dpcat237@gmail.com>
2018-11-17 21:41:35 +0200

    Add difference-of-squares

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,24 @@
+package diffsquares
+
+import "math"
+
+func SquareOfSum(n int) int {
+	sum := 0
+	for i := 0; i <= n; i++ {
+		sum += i
+	}
+
+	return int(math.Pow(float64(sum), 2))
+}
+
+func SumOfSquares(n int) int {
+	sum := 0
+	for i := 0; i <= n; i++ {
+		sum += int(math.Pow(float64(i), 2))
+	}
+	return sum
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
