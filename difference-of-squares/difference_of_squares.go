42da8d9a526720ec1f4e52ee4149452f36a91d7b
Denys Pasishnyi <dpcat237@gmail.com>
2018-11-18 11:29:56 +0200

    Improve performance of difference-of-squares

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,24 +1,17 @@
 package diffsquares
 
-import "math"
-
+// SquareOfSum the square of the sum of the first specified natural numbers
 func SquareOfSum(n int) int {
-	sum := 0
-	for i := 0; i <= n; i++ {
-		sum += i
-	}
-
-	return int(math.Pow(float64(sum), 2))
+	sum := n * (n + 1) / 2
+	return sum * sum
 }
 
+// SumOfSquares the sum of the squares of the first specified natural numbers
 func SumOfSquares(n int) int {
-	sum := 0
-	for i := 0; i <= n; i++ {
-		sum += int(math.Pow(float64(i), 2))
-	}
-	return sum
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
+// Difference returns difference between SquareOfSum and SumOfSquares
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
